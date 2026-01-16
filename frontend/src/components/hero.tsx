0dbf68f032f57713ee6255166aab647f7846a8cc
aditidangale <84914942+aditidangale@users.noreply.github.com>
2026-01-16 16:16:05 +0530

    Update hero.tsx

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -4,7 +4,7 @@ function hero() {
       <h1 className="cursor-text text-xl font-bold md:text-4xl">Journey Beyond Horizons</h1>
       <p className="my-4 cursor-text text-base md:text-lg">
         Dive into the world of travel with stories that transport you to far-off lands. Adventure
-        awaits around every corner. It's time to explore the world! aditi dangale...
+        awaits around every corner. It's time to explore the world! aditi dangale.
       </p>
       <div className="cursor-text text-sm font-semibold md:text-xl">Let's go!</div>
     </div>
