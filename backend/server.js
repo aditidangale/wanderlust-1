11d449ab83fd18676e22d4b1016bd97f8e5e7ab9
Ubuntu <ubuntu@ip-172-31-42-92.us-east-2.compute.internal>
2026-01-23 10:36:00 +0000

    Fix backend: add prom-client dependency and update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,16 @@ app.use(compression());
 
 //const client = require('prom-client');
 //const collectDefaultMetrics = client.collectDefaultMetrics;
-import client, { collectDefaultMetrics } from 'prom-client';
+import client from 'prom-client';
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics();
 
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
 });
+
 
 
 // Connect to database
